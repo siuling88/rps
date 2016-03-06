199f52badc2283e8c56709c61838a3eb26ed5cd1
Johanna Chan <8nahchan8@gmail.com>
2016-03-05 20:31:45 -0430

    Initial commit

---
+++ .idea/lcr.rb
@@ -0,0 +1,11 @@
+def LCR
+  attr_reader :value
+  def initialize(?)  # Indique los argumentos
+# Su c ́odigo aqu ́ı end
+  def each(p)
+# Su c ́odigo aqu ́ı
+  end
+  def solve
+# Su c ́odigo aqu ́ı
+  end
+end
