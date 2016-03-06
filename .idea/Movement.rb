950ad46600ea996160894896c79c54e9d4f8e769
Johanna Chan <8nahchan8@gmail.com>
2016-03-05 20:57:20 -0430

    Initial commit

--- .idea/Movement.rb
+++ .idea/Movement.rb
@@ -1,20 +1,20 @@
-
 class Movement
-  def to_s
-    puts "inhale and exhale"
+  module Funcion
+    def to_s
+      puts "You have four weeks in a month"
+    end
+    def score (m)
+      puts "You have 52 weeks in a year"
+    end
   end
 
-  def score()
-
-  end
+end
 
   class Rock < Movement
   end
 
   class Paper < Movement
-
   end
 
   class Scissors < Movement
-
   end
