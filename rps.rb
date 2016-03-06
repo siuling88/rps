bd6dadab315ebd5f0b4a659b4506cfc29afdaa1c
Johanna Chan <8nahchan8@gmail.com>
2016-03-05 23:14:01 -0430

    Initial commit

--- rps.rb
+++ rps.rb
@@ -4,40 +4,40 @@ class rps
   ##################################################### M O V E M E N T
 
   class Movement
+    att
     module Funcion
       def to_s
-        puts "You have four weeks in a month"
       end
       def score (m)
-        puts "You have 52 weeks in a year"
       end
     end
-
   end
 
   class Rock < Movement
+    def score (m)
+      e1.score && e2.score
+    end
   end
 
   class Paper < Movement
+    def score (m)
+    end
   end
 
   class Scissors < Movement
+    def score (m)
+    end
   end
 
   ##################################################### M O V E M E N T
-
-
   ##################################################### S T R A T E G Y
 
   class Strategy
     def next(m)
-
     end
     def to_s
-
     end
     def reset
-
     end
   end
 
@@ -46,35 +46,27 @@ class rps
   end
 
   class Biased < Strategy
-
   end
 
   class Mirror < Strategy
-
   end
 
   class Smart < Strategy
-
   end
 
   ##################################################### S T R A T E G Y
-
   ##################################################### M A T C H
 
   class Match
     def rounds(n)
-
     end
     def upto(n)
-
     end
     def restart
-
     end
   end
 
   ##################################################### M A T C H
-
   ##################################################### B I N T R E E
 
   class BinTree
@@ -90,7 +82,6 @@ class rps
   end
 
   ##################################################### B I N T R E E
-
   ##################################################### G R A P H N O D E
 
   class GraphNode
@@ -105,13 +96,12 @@ class rps
   end
 
   ##################################################### G R A P H N O D E
-
   ##################################################### L C R
 
   def LCR
     attr_reader :value
     def initialize(?)  # Indique los argumentos
-# Su c ́odigo aqu ́ı end
+# Su c ́odigo aqu ́ı
     def each(p)
 # Su c ́odigo aqu ́ı
     end
@@ -119,7 +109,7 @@ class rps
 # Su c ́odigo aqu ́ı
     end
     end
-
+  end
     ##################################################### L C R
 
-  end
+end
