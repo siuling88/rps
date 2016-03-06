43fb05a9138b68c9884a343923f7b37cb3518e6d
Johanna Chan <8nahchan8@gmail.com>
2016-03-05 19:57:48 -0430

    Initial commit

---
+++ .idea/Movement.rb
@@ -0,0 +1,20 @@
+
+class Movement
+  def to_s
+    puts "inhale and exhale"
+  end
+
+  def score()
+
+  end
+
+  class Rock < Movement
+  end
+
+  class Paper < Movement
+
+  end
+
+  class Scissors < Movement
+
+  end
