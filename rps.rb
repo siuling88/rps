9ccd66dec456814d7ad44054cf7f65dcedccffef
ZurdoRod <ZurdoRod@gmail.com>
2016-03-10 22:37:48 -0430

    added files

--- rps.rb
+++ rps.rb
@@ -1,71 +1,109 @@
 
-class rps
 
-  ##################################################### M O V E M E N T
+##################################################### M O V E M E N T
 
-  class Movement
-    att
-    module Funcion
-      def to_s
-      end
-      def score (m)
-      end
+class Movement
+  def to_s
+    self.class()
+  end
+end
+
+class Rock < Movement
+  def score (m)
+    if (m.is_a?(Rock))
+      [0,0]
+    elsif (m.is_a?(Paper))
+      [0,1]
+    else
+      [1,0]
     end
   end
+end
 
-  class Rock < Movement
-    def score (m)
-      e1.score && e2.score
+class Paper < Movement
+  def score (m)
+    if (m.is_a?(Paper))
+      [0,0]
+    elsif (m.is_a?(Scissors))
+      [0,1]
+    else
+      [1,0]
     end
   end
+end
 
-  class Paper < Movement
-    def score (m)
+class Scissors < Movement
+  def score (m)
+    if (m.is_a?(Scissors))
+      [0,0]
+    elsif (m.is_a?(Rock))
+      [0,1]
+    else
+      [1,0]
     end
   end
+end
 
-  class Scissors < Movement
-    def score (m)
+##################################################### M O V E M E N T
+##################################################### S T R A T E G Y
+
+class Strategy
+  SEMILLA = 42
+  def next(m)
+  end
+  def to_s
+  end
+  def reset
+  end
+end
+
+#  class Uniform < Strategy
+#   r = Uniform.new( [ :Rock, :Scissors, :Paper ] )
+#  end
+
+class Uniform < Strategy
+  attr_accessor :moves, :ran
+  def initialize (possibleMoves)
+    @moves = possibleMoves.uniq().select {|x| x.to_s == "Rock" or x.to_s == "Paper" or x.to_s == "Scissors"}
+    @ran = Random.new(seed = SEMILLA)
+    if @moves.empty?
+      raise 'No ha introducido ninguna estrategia válida'
     end
+    @moves
   end
 
-  ##################################################### M O V E M E N T
-  ##################################################### S T R A T E G Y
-
-  class Strategy
-    def next(m)
-    end
-    def to_s
-    end
-    def reset
-    end
+  def next(m)
+    move = Kernel.const_get(@moves[ran.rand(0..@moves.length-1)])
   end
 
-  class Uniform < Strategy
-    r = Uniform.new( [ :Rock, :Scissors, :Paper ] )
+  def reset
+    @ran = Random.new(seed = SEMILLA)
   end
 
-  class Biased < Strategy
+end
+
+
+class Biased < Strategy
+end
+
+class Mirror < Strategy
+end
+
+class Smart < Strategy
+end
+
+##################################################### S T R A T E G Y
+##################################################### M A T C H
+
+class Match
+  def rounds(n)
   end
-
-  class Mirror < Strategy
+  def upto(n)
   end
-
-  class Smart < Strategy
+  def restart
   end
-
-  ##################################################### S T R A T E G Y
-  ##################################################### M A T C H
-
-  class Match
-    def rounds(n)
-    end
-    def upto(n)
-    end
-    def restart
-    end
-  end
-
+end
+=begin
   ##################################################### M A T C H
   ##################################################### B I N T R E E
 
@@ -111,5 +149,4 @@ class rps
     end
   end
     ##################################################### L C R
-
-end
+=end
